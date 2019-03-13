ef17a0f94a197f4ef557672bba834487866db19e
Tyler Lawson <lawsont2@cse.msu.edu>
2019-03-13 01:28:07 -0400

    fixed all string outputs in table for boston sports

--- boston.py
+++ boston.py
@@ -1,32 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import re
 import pandas as pd
-import os
 from tabulate import tabulate
+from scrapeTools import get_first_table
 
-def fix_networks(doc):
-	cols = ["Game One", "Game Two", "Game Three"]
+def space_at_index(word, i):
+	return word[:i] + " " + word[i:]
+
+def fix_networks(word):
 	known_networks = ["NESN", "NBCSB"]
+	for network in known_networks:
+		if network in word:
+			ind = word.find(network)
+			word = space_at_index(word, ind)
+			break
+	return word
+
+def fix_middle(word):
+	words = ["at", "vs."]
+	before_word = word[:word.find("m.") - 2]
+	working_word = word[word.find("m.") - 2:word.find("m.") + 5]
+	after_word = word[word.find("m.") + 5:]
+	for a in words:
+		if a in working_word:
+			working_word = space_at_index(working_word, working_word.find(a))
+			break
+	return before_word + working_word + after_word
+
+def fix_rows(doc):
+	cols = ["Game One", "Game Two", "Game Three"]
 	for col in cols:
 		for i in range(len(doc[col])):
-			for network in known_networks:
-				if network in doc[col][i]:
-					ind = doc[col][i].find(network)
-					doc[col][i] = doc[col][i][:ind] + " " + doc[col][i][ind:]
+			doc[col][i] = fix_networks(doc[col][i])
+			doc[col][i] = fix_middle(doc[col][i])
 	return doc
 
 def get_boston_games():
 	url = "https://www.bostonglobe.com/sports/tvradio/listings"
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	driver = webdriver.Chrome(options=chrome_options)
-	driver.get(url)
-	soup = BeautifulSoup(driver.page_source, 'lxml')
-	table = soup.find_all("table")[0]
+	table = get_first_table(url)
 	df = pd.read_html(str(table))[0]
 	df.columns = ["Team", "Game One", "Game Two", "Game Three"]
 	document = df.to_dict(orient='list')
-	document = fix_networks(document)
+	document = fix_rows(document)
 	return "Local teams' next 3 games:\n" + tabulate(document, headers='keys')
--- scrapeTools.py
+++ scrapeTools.py
@@ -1,9 +1,27 @@
 from six.moves.urllib.request import urlopen
 import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def get_first_table(url):
 	page = urlopen(url)
 	# parse the html using beautiful soup and store in variable `soup`
 	soup = BeautifulSoup(page, 'html.parser')
-	return soup.find_all("table")[0]
+	found = True
+	try:
+		return soup.find_all("table")[0]
+	except IndexError:
+		try:
+			chrome_options = Options()
+			chrome_options.add_argument("--headless")
+			driver = webdriver.Chrome(options=chrome_options)
+			driver.get(url)
+			soup = BeautifulSoup(driver.page_source, 'lxml')
+			driver.close()
+			return soup.find_all("table")[0]
+		except:
+			found = False
+			
+	if not found:
+		raise ValueError("No table found")
