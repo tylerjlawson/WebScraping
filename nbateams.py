9f9448b06b572f012e55d0e9f24b8d66e3be2258
Tyler Lawson <lawsont2@cse.msu.edu>
2019-03-12 20:27:35 -0400

    made module for basic scraping tools

--- nbateams.py
+++ nbateams.py
@@ -1,22 +1,15 @@
 from six.moves.urllib.request import urlopen
 import pandas as pd
 from bs4 import BeautifulSoup, SoupStrainer
+from scrapeTools import get_first_table
 
 def nba_team_names():
-	ball_page = "https://www.foxsports.com/nba/standings?season=2018&seasonType=1&grouping=3&advanced=0"
-	# query the website and return the html to the variable ‘page’
-	page = urlopen(ball_page)
-	# parse the html using beautiful soup and store in variable `soup`
-	soup = BeautifulSoup(page, 'html.parser')
-	table = soup.find_all("table", attrs={"class": "wisbb_standardTable"})[0]
+	table = get_first_table("https://www.foxsports.com/nba/standings?season=2018&seasonType=1&grouping=3&advanced=0")
 	df = pd.read_html(str(table))[0]
 	return df["NBA"]
 
 def get_east_west():
-	page = "https://www.foxsports.com/nba/standings?season=2018&seasonType=1&grouping=1&advanced=0"
-	url = urlopen(page)
-	soup = BeautifulSoup(url, 'html.parser')
-	table = soup.find_all("table", attrs={"class": "wisbb_standardTable"})[0]
+	table = get_first_table("https://www.foxsports.com/nba/standings?season=2018&seasonType=1&grouping=1&advanced=0")
 	df = pd.read_html(str(table))[0]
 	east = list(df.iterrows())[0:15]
 	west = list(df.iterrows())[17:]
@@ -33,4 +26,3 @@ def east_vs_west():
 		WL[1] += total_losses - int(team_conf_rec[1])
 
 	return "East-West\n" + "-".join([str(i) for i in WL])
-
---
+++ scrapeTools.py
@@ -0,0 +1,9 @@
+from six.moves.urllib.request import urlopen
+import pandas as pd
+from bs4 import BeautifulSoup, SoupStrainer
+
+def get_first_table(url):
+	page = urlopen(url)
+	# parse the html using beautiful soup and store in variable `soup`
+	soup = BeautifulSoup(page, 'html.parser')
+	return soup.find_all("table")[0]
