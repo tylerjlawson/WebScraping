de45cb786feefc03606bd2c70e17f5594f1807dc
Tyler Lawson <tyler@pesky.localdomain>
2019-03-13 18:53:11 -0400

    progress made

--- screenshotter.py
+++ screenshotter.py
@@ -1,26 +1,61 @@
 from six.moves.urllib.request import urlopen
 from bs4 import BeautifulSoup
 from lxml import html
-from re import compile
+import urllib
+from imgkit import from_url
 
+def remove_last_slash(S):
+	if S[-1] == "/":
+		return S[:-1]
+	return S
 
-def get_links_on_page(base_url):
-	page = urlopen(base_url)
+def get_links_on_page(base_url, curr):
+	links = []
+	try:
+		page = urlopen(base_url)
+	except urllib.error.HTTPError:
+		return []
 	soup = BeautifulSoup(page, "html.parser")
-	pattern = compile("base_url$ + [\S]")
 
-	if base_url[-1] == "/":
-		base_url = base_url[:-1]
+	base_url = remove_last_slash(base_url)
+	curr = remove_last_slash(curr)
 
 	for link in soup.find_all('a',href=True):
 		the_link = link['href']
-		if pattern.match(the_link):
-			print (the_link)
+		if base_url in the_link:
+			links.append(the_link)
 		elif the_link[0] == "/":
-			print(base_url + the_link)
+			links.append(base_url + the_link)
+	return links
 
-def get_all_links(base_url, links):
+def get_all_links(base_url, curr_url, links, visited=[]):
+	if curr_url in visited:
+		return []
+	else:
+		visited.append(curr_url)
+		temp = links`	1
+		for link in links:
+			temp += get_links_on_page(base_url, link)
+			sub_links = get_all_links(base_url, link, [], visited)
+			temp = list(set(temp + sub_links))
+	return links
+
+def remove_extension(S):
+	return S.split(".")[0]
+
+def collect_pdf(site, save_dir):
+	links = get_all_links(site,site,[site])
+	for link in links: print(link)
+	# 	if link == site:
+	# 		filename = save_dir + "/home.jpg"
+	# 	else:
+	# 		n = save_dir[-1] == "/"
+	# 		if link[-1] == "/": link = link[:-1]
+	# 		filename = save_dir + remove_extension(link[link.rfind("/") + n:]) + ".jpg"
+	# 	try:
+	# 		from_url(link, filename)
+	# 	except:
+	# 		pass
 
 
-
-get_links_on_page("http://theplpt.com/")
+collect_pdf("http://theplpt.com/", "/home/tyler/Documents/hello")
