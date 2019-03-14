646fcaafcb6a057c532a76e33132d14a44f36297
Tyler Lawson <lawsont2@cse.msu.edu>
2019-03-13 20:17:20 -0400

    done with screenshotter, I wish it worked better, but oh well

--- screenshotter.py
+++ screenshotter.py
@@ -2,7 +2,11 @@ from six.moves.urllib.request import urlopen
 from bs4 import BeautifulSoup
 from lxml import html
 import urllib
-from imgkit import from_url
+import imgkit
+from time import sleep
+
+config_path = 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltoimage.exe'
+config = imgkit.config(wkhtmltoimage=config_path)
 
 def remove_last_slash(S):
 	if S[-1] == "/":
@@ -12,9 +16,10 @@ def remove_last_slash(S):
 def get_links_on_page(base_url, curr):
 	links = []
 	try:
-		page = urlopen(base_url)
+		page = urlopen(curr)
 	except urllib.error.HTTPError:
 		return []
+
 	soup = BeautifulSoup(page, "html.parser")
 
 	base_url = remove_last_slash(base_url)
@@ -22,40 +27,42 @@ def get_links_on_page(base_url, curr):
 
 	for link in soup.find_all('a',href=True):
 		the_link = link['href']
-		if base_url in the_link:
-			links.append(the_link)
-		elif the_link[0] == "/":
-			links.append(base_url + the_link)
+		if the_link != "":
+			if base_url in the_link:
+				links.append(the_link)
+			elif the_link[0] == "/":
+				links.append(base_url + the_link)
 	return links
 
-def get_all_links(base_url, curr_url, links, visited=[]):
+def get_all_links(base_url, curr_url, visited=[]):
 	if curr_url in visited:
 		return []
 	else:
+		sleep(2)
 		visited.append(curr_url)
-		temp = links`	1
-		for link in links:
-			temp += get_links_on_page(base_url, link)
-			sub_links = get_all_links(base_url, link, [], visited)
-			temp = list(set(temp + sub_links))
-	return links
+		try:
+			sub_links = get_links_on_page(base_url, curr_url)
+		except:
+			sub_links = []
+		for link in sub_links:
+			get_all_links(base_url, link, visited)
+	return visited
 
 def remove_extension(S):
 	return S.split(".")[0]
 
-def collect_pdf(site, save_dir):
-	links = get_all_links(site,site,[site])
-	for link in links: print(link)
-	# 	if link == site:
-	# 		filename = save_dir + "/home.jpg"
-	# 	else:
-	# 		n = save_dir[-1] == "/"
-	# 		if link[-1] == "/": link = link[:-1]
-	# 		filename = save_dir + remove_extension(link[link.rfind("/") + n:]) + ".jpg"
-	# 	try:
-	# 		from_url(link, filename)
-	# 	except:
-	# 		pass
-
-
-collect_pdf("http://theplpt.com/", "/home/tyler/Documents/hello")
+def collect_png(site, save_dir):
+	links = get_all_links(site,site)
+	for link in links:
+		if link == site:
+			filename = save_dir + "/home.png"
+		else:
+			n = save_dir[-1] == "/"
+			if link[-1] == "/": link = link[:-1]
+			filename = save_dir + remove_extension(link[link.rfind("/") + n:]) + ".png"
+		try:
+			print((link, filename))
+			options = {'format': 'png', 'width': 1080, 'disable-smart-width': ''}
+			imgkit.from_url(link, filename, config=config, options = options)
+		except:
+			pass
